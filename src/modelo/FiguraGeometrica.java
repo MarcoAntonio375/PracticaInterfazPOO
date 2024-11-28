f03ad5292ac6274f87667558dfbb7a65cbb21b51
MarcoAntonio375 <marco.tovar895@ist17dejulio.edu.ec>
2024-11-28 16:04:28 -0500

    Figura geometrica

---
+++ src/modelo/FiguraGeometrica.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author CASA PC
+ */
+public interface FiguraGeometrica {
+    //decarar constantes-> final
+    public final double PI=3.1416; 
+    
+    //declarar metodossin implementarios
+    public void area();
+    public double perimetro();
+    
+}
